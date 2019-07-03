4b52bb90d3fc3cb1b6b66874c0934536f6cab587
Chenyang Tang <chenyang.tang@microfocus.com>
2019-07-03 18:38:40 +0800

    experiment

--- src/examples/tone/reconciler.stories.js
+++ src/examples/tone/reconciler.stories.js
@@ -5,13 +5,17 @@ import ToneRoot from './Tone';
 
 const Example = () => {
   const [state, setState] = useState(false);
+  const synth = useRef();
   const onClick = React.useCallback(() => {
-    setState((state => !state));
+    setState(state => !state);
   }, []);
   const trigger = React.useCallback((inst, time, note) => {
     console.log(Tone.Transport.position);
     inst.triggerAttackRelease(note, '8n', time);
   }, []);
+  useEffect(() => {
+    console.log(synth.current);
+  });
   return (
     <>
       <div>Hello</div>
@@ -19,18 +23,23 @@ const Example = () => {
       <ToneRoot start={state}>
         <distortion distortion={1}>
           <feedbackDelay delay="4n" feedback={0.2}>
-            <synth>
+            <synth ref={synth}>
               {/* {state ? <triggerAttackRelease args={['C4', '4n']} /> : null} */}
-              <sequence args={[trigger, ['C5', 'C4', 'C3', 'C2'], '4n']} />
+              {/* <sequence args={[trigger, ['C5', 'C4', 'C3', 'C2'], '4n']} /> */}
+              <part
+                args={[trigger, [[0, 'B4'], ['0:2', 'C5'], ['0:3:2', 'D5']]]}
+              />
             </synth>
           </feedbackDelay>
         </distortion>
+        <membraneSynth>
+          <part args={[trigger, [[0, 'C2'], ['0:2', 'C2'], ['0:3:2', 'C2']]]} />
+        </membraneSynth>
       </ToneRoot>
     </>
   );
-}
+};
 
-storiesOf('Tone Reconciler', module)
-  .add('hello', () => {
-    return <Example />;
-  });
+storiesOf('Tone Reconciler', module).add('hello', () => {
+  return <Example />;
+});
